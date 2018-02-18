522c2d617d56c4ce4be36ed24e21a12dd93f71a9
Sander Mertens <sander.mertens8@gmail.com>
2018-02-18 13:47:00 -0800

    Update to new language binding

--- include/test.h
+++ include/test.h
@@ -1,5 +1,4 @@
 /* test.h
- *
  * This is the main package file. Include this file in other projects.
  * Only modify inside the header-end and body-end sections.
  */
@@ -28,6 +27,7 @@ corto_string test_command(corto_id buffer, corto_string lib, corto_object testca
 #include <corto/test/_type.h>
 #include <corto/test/_interface.h>
 #include <corto/test/_load.h>
+#include <corto/test/_binding.h>
 #include <corto/test/c/_api.h>
 
 /* $body() */
